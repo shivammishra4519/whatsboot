0f9e957841b72b6623517161e5dfbc4751aeb1c6
shivammishra4519 <misra3408@gmail.com>
2024-08-26 17:14:48 +0530

    sd4

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const server = http.createServer(app);
 const io = socketIo(server);
 // app.use(cors());
 app.use(cors({
-    origin: '*', // Your Angular app URL
+    origin: 'http://62.72.56.135/', // Your Angular app URL
     credentials: true // Allow cookies to be sent and received
   }));
 app.use('/user',users);
